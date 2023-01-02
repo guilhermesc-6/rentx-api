2204c2b95d8621d7eac3404fd15eb957df6f8c86
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2023-01-02 12:25:44 -0300

    update swagger documentation.

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -12,6 +12,6 @@ export class ImportCategoryController{
 
     await importCategoryUseCase.execute(file);
 
-    return (await response).send();
+    return (await response).status(201).send();
   }
 }
