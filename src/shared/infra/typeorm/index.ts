e2e10770df7ba7e0699bfa942eb298c7e4d8073d
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2023-01-05 13:42:55 -0300

    Add: Cars table.

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-import { User } from "@modules/accounts/infra/typeorm/entities/User";
-import { Category } from "@modules/cars/infra/typeorm/entities/Category";
-import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
+import { User } from "../../../modules/accounts/infra/typeorm/entities/User";
+import { Category } from "../../../modules/cars/infra/typeorm/entities/Category";
+import { Specification } from "../../../modules/cars/infra/typeorm/entities/Specification";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -12,6 +12,6 @@ export const AppDataSource = new DataSource({
   username: "docker",
   password: "ignite",
   database: "rentx",
-  migrations: ["./src/shared/infra/database/migrations/*.{ts,js}"],
+  migrations: ["./src/shared/infra/typeorm/migrations/*.{ts,js}"],
   entities: [Category, Specification, User]
 });
